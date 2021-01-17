c9782ebb6c3aad13d39bd55e83b21199cdb8dacd
Klod <c.buisson@it-student.fr>
2021-01-18 00:20:03 +0100

    pipage login et register

--- controller/login_controller.php
+++ controller/login_controller.php
@@ -1,5 +1,5 @@
 <?php
-include '../model/user.php';
+include_once '../model/user.php';
 // $_SESSION['connected'] = false;
 
 // $passwordHash = '$2y$10$USjE0V9IPIB5chbQr7XJLe0X5jdX2c8W5zOyV2h3rP8t3Xt2PbSie';
@@ -17,38 +17,40 @@ if (isset($_POST['email'])) {
     // $firstname = getEmailPassword($pdo, $_POST['email'])['first_name'];
     // var_dump($firstname);
 
-$dataSession= getEmailPassword($pdo, $_POST['email']);
-var_dump ($dataSession);
-$_SESSION['email'] = $dataSession['email'];
-$verifiedEmail = $dataSession['email'];
-$passwordHash = $dataSession['password'];
-$firstname = $dataSession['first_name'];
+    $dataSession= getEmailPassword($pdo, $_POST['email']);
+    var_dump ($dataSession);
+        if($dataSession!== false){
+        $_SESSION['email'] = $dataSession['email'];
+        $verifiedEmail = $dataSession['email'];
+        $passwordHash = $dataSession['password'];
+        $firstname = $dataSession['first_name'];
 
-    if (password_verify($_POST['password'], $passwordHash) && $_POST['email'] === $verifiedEmail) {
+        if (password_verify($_POST['password'], $passwordHash) && $_POST['email'] === $verifiedEmail) {
+            $_SESSION['connected'] = true;
+            $clientInfos = getClient($pdo, $_SESSION['email']);
+            $_SESSION['id'] = $clientInfos['id'];
+            $_SESSION['first_name'] = $clientInfos['first_name'];
+            $_SESSION['last_name'] = $clientInfos['last_name'];
+            $_SESSION['phone'] = $clientInfos['phone'];
+            
+            echo "<p>Session connecté</p>";
+            echo "<p>Bonjour " . $firstname . '</p>';
+            
 
-
-        $_SESSION['connected'] = true;
-        $clientInfos = getClient($pdo, $_SESSION['email']);
-        $_SESSION['id'] = $clientInfos['id'];
-        $_SESSION['first_name'] = $clientInfos['first_name'];
-        $_SESSION['last_name'] = $clientInfos['last_name'];
-        $_SESSION['phone'] = $clientInfos['phone'];
-        
-        echo "<p>Session connecté</p>";
-        echo "<p>Bonjour " . $firstname . '</p>';
-        // header("Location: /profil");
-
-        // $my_logs = fopen('../logs/' . $_SESSION['date'] . 'logs.txt', 'a+');
-        // fputs($my_logs, $_SESSION['email'] . ' session connectée' . $_SERVER['REQUEST_URI'] . $dateTime . "\n");
-        header('Location: item');
-        exit();
-    } else {
+            // $my_logs = fopen('../logs/' . $_SESSION['date'] . 'logs.txt', 'a+');
+            // fputs($my_logs, $_SESSION['email'] . ' session connectée' . $_SERVER['REQUEST_URI'] . $dateTime . "\n");
+            header('Location: item');
+            exit();
+        } else {
+            $_SESSION['connected'] = false;
+            echo "mot de passe invalide" . '<br>';
+            // $my_logs = fopen('../logs/' . $_SESSION['date'] . 'logs.txt', 'a+');
+            // fputs($my_logs, $_POST['email'] . ' a tenté de se connecter ' . $_SERVER['REQUEST_URI'] . $dateTime . "\n");
+        }
+    }else{
         $_SESSION['connected'] = false;
-        echo "mot de passe invalide" . '<br>';
-        $my_logs = fopen('../logs/' . $_SESSION['date'] . 'logs.txt', 'a+');
-        fputs($my_logs, $_POST['email'] . ' a tenté de se connecter ' . $_SERVER['REQUEST_URI'] . $dateTime . "\n");
+        echo "Email inconnu" . '<br>';
     }
-    
 }
 if (isset($_SESSION['connected'])&& $_SESSION['connected']=== true){
     header("Location: account");
--- controller/register_controller.php
+++ controller/register_controller.php
@@ -1,21 +1,58 @@
 <?php
 
-
+include_once '../model/user.php';
 
 if (empty($_POST['email']) === false && empty($_POST['password']) === false && empty($_POST['lastname']) === false){
-    if(isset($_POST['email'])){
+        $dataSession= getEmailPassword($pdo, $_POST['email']);
+        var_dump ($dataSession);
+        if($dataSession!== false){
+            $_SESSION['email'] = $dataSession['email'];
+            $verifiedEmail = $dataSession['email'];
+            $passwordHash = $dataSession['password'];
+            $firstname = $dataSession['first_name'];
+
+            if (password_verify($_POST['password'], $passwordHash) && $_POST['email'] === $verifiedEmail) {
+
+                $_SESSION['connected'] = true;
+                $clientInfos = getClient($pdo, $_SESSION['email']);
+                $_SESSION['id'] = $clientInfos['id'];
+                $_SESSION['first_name'] = $clientInfos['first_name'];
+                $_SESSION['last_name'] = $clientInfos['last_name'];
+                $_SESSION['phone'] = $clientInfos['phone'];
+                var_dump($clientInfos);
+                echo "<p>Session connecté</p>";
+                echo "<p>Bonjour " . $firstname . '</p>';
+                // header("Location: /profil");
+
+                // $my_logs = fopen('../logs/' . $_SESSION['date'] . 'logs.txt', 'a+');
+                // fputs($my_logs, $_SESSION['email'] . ' session connectée' . $_SERVER['REQUEST_URI'] . $dateTime . "\n");
+                // header('Location: item');
+                // exit();
+                // }
+                header('Location: account');
+                exit();
+            }else{
+                echo "Cet email excite déjà " . '<br>';
+            }
+        }
+
+    if(isset($_POST['email']) ){
         $_POST['firstname']= trim($_POST['firstname']);
         $_POST['lastname']= trim($_POST['lastname']);
         $_POST['email']= trim($_POST['email']);
         $_POST['phone']= trim($_POST['phone']);
         $_POST['password']= trim($_POST['password']);
         $passwordHash = password_hash(trim($_POST['password']),PASSWORD_BCRYPT);
-$_POST['password'] = $passwordHash;
+        $_POST['password'] = $passwordHash;
 
-        var_dump(addUser($pdo, $_POST));
+        addUser($pdo, $_POST);
+        
+        // if (password_verify($_POST['password'], $passwordHash) && $_POST['email'] === $verifiedEmail) {
+        
     }
 }
-// var_dump($_POST);
+var_dump($_POST);
+var_dump($_SESSION);
 
 
 
